f1e0edb0d1f1852e349aec7cbaa964f12b683e6a
Naseer Dari <naseerdari01@gmail.com>
2015-10-03 00:26:33 -0500

    A new approach
    This commit marks a new approach. I am no longer going to worry about
    making a mat64 object. rather, I will rely on the builtin slices,
    (specifically the 2D slices of [][]float64), and create functions which
    act on them. This will be more extensible, i believe.

--- mat64.go
+++ mat64.go
@@ -1,5 +1,5 @@
-// Package Mat64 contains a float64 Matrix object for Go.
-package Mat64
+// Package mat64 contains a float64 Matrix object for Go.
+package mat64
 
 import (
 	"log"
@@ -8,87 +8,58 @@ import (
 )
 
 var (
-	errColInx   = "Mat64.%s Error: Column index %d is out of range"
-	errRowInx   = "Mat64.%s Error: Row index %d is out of range"
-	errMismatch = "Mat64.%s Error: Shape mismatch of the matreces"
+	errMismatch = "mat64.%s Error: Shape mismatch of the slices"
 )
-
-type Mat64 struct {
-	NumRows int
-	NumCols int
-	Vals    []float64
-}
 
 // ElementalFn is a function that takes a float64 and returns a
 // float64. This function can therefore be applied to each element
-// of a Mat64, and can be used to construct a new transformed Mat64.
+// of a 2D float64 slice, and can be used to construct a new one.
 type ElementalFn func(float64) float64
 
-// New returns a Mat64 object with the given rows and cols
-func New(r, c int) *Mat64 {
-	return &Mat64{
-		NumRows: r,
-		NumCols: c,
-		Vals:    make([]float64, r*c),
+// New returns a 2D slice of float64s with the given row and columns.
+func New(r, c int) [][]float64 {
+	arr := make([][]float64, r)
+	for i := 0; i < r; i++ {
+		arr[i] = make([]float64, c)
 	}
+	return arr
 }
 
-// Identity returns an r by r identity matrix for a given r.
-func Identity(r int) *Mat64 {
+// I returns an r by r identity matrix for a given r.
+func I(r int) [][]float64 {
 	identity := New(r, r)
 	for i := 0; i < r; i++ {
-		identity.Vals[i*r+i] = 1.0
+		identity[i][i] = 1.0
 	}
 	return identity
 }
 
-// Col returns a Mat64, representing a single column of the
-// original mat64 object at a given location.
-func (m *Mat64) Col(c int) *Mat64 {
-	if c >= m.NumCols {
-		log.Fatalf(errColInx, "Col", c)
-	}
-	vec := New(m.NumRows, 1)
-	for i := 0; i < m.NumRows; i++ {
-		vec.Vals[i] = m.Vals[i*m.NumCols+c]
+// Col returns a column of a 2D slice of float64s.
+func Col(c int, m [][]float64) []float64 {
+	vec := make([]float64, len(m))
+	for r := 0; r < len(m); r++ {
+		vec[r] = m[r][c]
 	}
 	return vec
 }
 
-// Row returns a Mat64, representing a single row of the
-// original Mat64 object at the give location.
-func (m *Mat64) Row(r int) *Mat64 {
-	if r >= m.NumRows {
-		log.Fatalf(errRowInx, "Row", r)
-	}
-	vec := New(1, m.NumCols)
-	for i := 0; i < m.NumCols; i++ {
-		vec.Vals[i] = m.Vals[r*m.NumCols+i]
+// Row returns a row of a 2D slice of float64s
+func Row(r int, m [][]float64) []float64 {
+	vec := make([]float64, len(m[r]))
+	for c := 0; c < len(m[r]); c++ {
+		vec[c] = m[r][c]
 	}
 	return vec
 }
 
-// At returns the values of the entry in an Mat64 object at
-// the specified row and col. It throws errors if an index is out
-// of range.
-func (m *Mat64) At(r, c int) float64 {
-	if r >= m.NumRows {
-		log.Fatalf(errRowInx, "At", r)
-	}
-	if c > m.NumCols {
-		log.Fatalf(errColInx, "At", c)
-	}
-	return m.Vals[r*m.NumCols+c]
-}
-
-// Transpose returns a copy of a given matrix with the elements
+// T returns a copy of a given matrix with the elements
 // mirrored across the diagonal. for example, the element At(i, j) becomes the
 // element At(j, i). This function leaves the original matrix intact.
-func (m *Mat64) Transpose() *Mat64 {
-	transpose := New(m.NumCols, m.NumRows)
-	for i := 0; i < m.NumRows; i++ {
-		for j := 0; j < m.NumCols; j++ {
-			transpose.Vals[j*m.NumRows+i] = m.At(i, j)
+func T(m [][]float64) [][]float64 {
+	transpose := New(len(m[0]), len(m))
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[i]); j++ {
+			transpose[j][i] = m[i][j]
 		}
 	}
 	return transpose
@@ -96,122 +67,106 @@ func (m *Mat64) Transpose() *Mat64 {
 
 // Equals checks if two mat objects have the same shape and the
 // same entries in each row and column.
-func (m *Mat64) Equals(n *Mat64) bool {
-	if m.NumRows != n.NumRows || m.NumCols != m.NumCols {
-		log.Fatalf(errMismatch, "Equals")
+func Equal(m, n [][]float64) bool {
+	if len(m) != len(n) {
+		return false
 	}
-	for i := 0; i < m.NumCols*m.NumRows; i++ {
-		if m.Vals[i] != n.Vals[i] {
+	for i := 0; i < len(m); i++ {
+		if len(m[i]) != len(n[i]) {
 			return false
+		}
+		for j := 0; j < len(m[i]); j++ {
+			if m[i][j] != n[i][j] {
+				return false
+			}
 		}
 	}
 	return true
 }
 
-// Times returns a new matrix that is the result of
-// element-wise multiplication of the matrix by another, leaving
-// both original matrices intact.
-func (m *Mat64) Times(n *Mat64) *Mat64 {
-	if m.NumRows != n.NumRows || m.NumCols != m.NumCols {
+// Times returns a new 2D slice that is the result of
+// element-wise multiplication of two 2D slices.
+func Times(m, n [][]float64) [][]float64 {
+	if len(m) != len(n) {
 		log.Fatalf(errMismatch, "Times")
 	}
-	o := New(m.NumRows, m.NumCols)
-	for i := 0; i < m.NumCols*m.NumRows; i++ {
-		o.Vals[i] = m.Vals[i] * n.Vals[i]
+	o := make([][]float64, len(m))
+	for i := 0; i < len(m); i++ {
+		if len(m[i]) != len(n[i]) {
+			log.Fatalf(errMismatch, "Times")
+		}
+		o[i] = make([]float64, len(m[i]))
+		for j := 0; j < len(m[i]); j++ {
+			o[i][j] = m[i][j] * n[i][j]
+		}
 	}
 	return o
 }
 
-// TimesInPlace multiplies a Mat64 by another in place. This means that
-// the original matrix is lost.
-func (m *Mat64) TimesInPlace(n *Mat64) *Mat64 {
-	if m.NumRows != n.NumRows || m.NumCols != m.NumCols {
-		log.Fatalf(errMismatch, "Times")
-	}
-	for i := 0; i < m.NumCols*m.NumRows; i++ {
-		m.Vals[i] *= n.Vals[i]
-	}
-	return m
-}
-
 // Apply calls a given Elemental function on each Element
-// of a matrix, returning a new transformed matrix.
-func (m *Mat64) Apply(f ElementalFn) *Mat64 {
-	n := New(m.NumRows, m.NumCols)
-	for i := 0; i < m.NumRows*m.NumCols; i++ {
-		n.Vals[i] = f(m.Vals[i])
-	}
-	return n
-}
-
-// ApplyInPlace calls a given Elemental function on each Element
-// of a matrix, and then returns the transformed matrix.
-func (m *Mat64) ApplyInPlace(f ElementalFn) *Mat64 {
-	for i := 0; i < m.NumRows*m.NumCols; i++ {
-		m.Vals[i] = f(m.Vals[i])
+// of a 2D slice, returning it afterwards.
+func Apply(f ElementalFn, m [][]float64) [][]float64 {
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[i]); j++ {
+			m[i][j] = f(m[i][j])
+		}
 	}
 	return m
 }
 
-// Dot is the matrix multiplication of two Mat64 objects. Consider
-// The following two mat64 objects, pretty printed for illusration:
-//
-// A = [[1, 0],
-//      [0, 1]]
-//
-// and
-//
-// B = [[4, 1],
-//      [2, 2]]
-//
-// A.Dot(B) = [[4, 1],
-//             [2, 2]]
-//
-// The number of elements in the first matrix row, must equal the number
-// elements in the second matrix column.
-func (m *Mat64) Dot(n *Mat64) *Mat64 {
-	if m.NumCols != n.NumRows {
-		log.Fatalf(errMismatch, "Dot")
+// Dot is the matrix multiplication of two 2D slices of float64s
+
+func Dot(m, n [][]float64) [][]float64 {
+	lenm := len(m)
+	// make sure that the length of the row of m matches the length of
+	// each column in n.
+	for i := 0; i < len(n); i++ {
+		if lenm != len(n[i]) {
+			log.Fatalf(errMismatch, "Dot")
+		}
 	}
-	o := New(m.NumRows, n.NumCols)
-	items := m.NumCols
-	for i := 0; i < m.NumRows; i++ {
-		for j := 0; j < n.NumCols; j++ {
-			for k := 0; k < items; k++ {
-				o.Vals[i*o.NumRows+j] += (m.At(i, k) * n.At(k, j))
+	o := make([][]float64, len(m))
+	for i := 0; i < len(m); i++ {
+		if len(m[i]) != len(n) {
+			log.Fatalf(errMismatch, "Dot")
+		}
+		o[i] = make([]float64, len(n[0]))
+		for j := 0; j < len(m[i]); j++ {
+			for k := 0; k < len(n); k++ {
+				o[i][j] += m[i][k] * n[k][j]
 			}
 		}
 	}
 	return o
 }
 
-// Reset puts all the elements of a Mat64 values set to 0.0.
-func (m *Mat64) Reset() *Mat64 {
-	return m.ApplyInPlace(func(i float64) float64 { return 0.0 })
+// Reset puts all the elements of a mat64 values set to 0.0.
+func Reset(m [][]float64) [][]float64 {
+	return Apply(func(i float64) float64 { return 0.0 }, m)
 }
 
-// Dump prints the content of a Mat64 object to a file, using the given
+// Dump prints the content of a mat64 object to a file, using the given
 // delimeter between the elements of a row, and a new line between rows.
 // For instance, giving the comma (",") as a delimiter will essentially
-// creates a csv file from the Mat64 object.
-func (m *Mat64) Dump(fileName, delemiter string) {
+// creates a csv file from the mat64 object.
+func Dump(m [][]float64, fileName, delimiter string) {
 	var str string
-	for i := 0; i < m.NumRows; i++ {
-		for j := 0; j < m.NumCols; j++ {
-			str += strconv.FormatFloat(m.Vals[i*m.NumRows+j], 'f', 14, 64)
-			str += delimeter
+	for i := 0; i < len(m); i++ {
+		for j := 0; j < len(m[i]); j++ {
+			str += strconv.FormatFloat(m[i][j], 'f', 14, 64)
+			str += delimiter
 		}
-		if i+1 != m.NumRows {
+		if i+1 != len(m) {
 			str += "\n"
 		}
 	}
 	f, err := os.Create(fileName)
 	if err != nil {
-		log.Fatalf(err)
+		log.Fatalf("Cannot open %v: %v", fileName, err)
 	}
 	defer f.Close()
 	_, err = f.WriteString(str)
 	if err != nil {
-		log.Fatalf(err)
+		log.Fatalf("Cannot write to %v: %v", fileName, err)
 	}
 }
--- mat64_test.go
+++ mat64_test.go
@@ -1,24 +1,23 @@
-package Mat64
+package mat64
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestIdentity(t *testing.T) {
+func TestI(t *testing.T) {
 	var (
 		row = 4
 	)
-	m := Identity(row)
+	m := I(row)
 	for i := 0; i < row; i++ {
 		for j := 0; j < row; j++ {
 			if i == j {
-				if m.Vals[i*row+j] != 1.0 {
-					t.Errorf("I[%v,%v] == %v, want 1.0", i, j, m.Vals[i*4+j])
+				if m[i][j] != 1.0 {
+					t.Errorf("I[%v,%v] == %v, want 1.0", i, j, m[i][j])
 				}
 			} else {
-				if m.Vals[i*row+j] != 0.0 {
-					t.Errorf("I[%v,%v] == %v, want 0.0", i, j, m.Vals[i*4+j])
+				if m[i][j] != 0.0 {
+					t.Errorf("I[%v,%v] == %v, want 0.0", i, j, m[i][j])
 				}
 			}
 		}
@@ -31,21 +30,21 @@ func TestCol(t *testing.T) {
 		col = 4
 	)
 	m := New(row, col)
-	for i := 0; i < row*col; i++ {
-		m.Vals[i] = float64(i)
+	iter := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			m[i][j] = float64(iter)
+			iter++
+		}
 	}
-	got := m.Col(2)
-	if got.NumRows != row {
-		t.Errorf("got.NumRows == %v, want %v", got.NumRows, row)
+	got := Col(2, m)
+	if len(got) != row {
+		t.Errorf("got.NumRows == %v, want %v", len(got), row)
 	}
-	if got.NumCols != 1 {
-		t.Errorf("got.NumCols == %v, want 1", got.NumCols)
-	}
-
 	want := []float64{2.0, 6.0, 10.0}
 	for i := 0; i < row; i++ {
-		if want[i] != got.Vals[i] {
-			t.Errorf("m.Col(2][%v] == %v, want %v", i, got.Vals[i], want[i])
+		if want[i] != got[i] {
+			t.Errorf("m[%v][2] == %v, want %v", i, got[i], want[i])
 		}
 	}
 }
@@ -56,48 +55,40 @@ func TestRow(t *testing.T) {
 		col = 4
 	)
 	m := New(row, col)
-	for i := 0; i < row*col; i++ {
-		m.Vals[i] = float64(i)
+	iter := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			m[i][j] = float64(iter)
+			iter++
+		}
 	}
-	got := m.Row(1)
-	if got.NumCols != col {
-		t.Errorf("got.NumCols == %v, want 4", got.NumCols)
+	got := Row(1, m)
+	if len(got) != col {
+		t.Errorf("len(got) == %v, want %v", len(got), col)
 	}
 	want := []float64{4.0, 5.0, 6.0, 7.0}
 	for i := 0; i < col; i++ {
-		if want[i] != got.Vals[i] {
-			t.Errorf("got.Vals[%v] is %v, want %v", i, got.Vals[i], want[i])
+		if want[i] != got[i] {
+			t.Errorf("m[1][%v] is %v, want %v", i, got[i], want[i])
 		}
 	}
 }
 
-func TestAt(t *testing.T) {
-	m := New(3, 4)
-	for i := 0; i < 12; i++ {
-		m.Vals[i] = float64(i)
-	}
-	got := m.At(2, 1)
-	if got != 9.0 {
-		t.Errorf("got %v, want 9.0", got)
-	}
-}
-
-func TestTranspose(t *testing.T) {
+func TestT(t *testing.T) {
 	var (
 		row = 5
 		col = 7
 	)
 	m := New(row, col)
-	n := m.Transpose()
+	n := T(m)
 	for i := 0; i < row; i++ {
 		for j := 0; j < col; j++ {
-			if n.At(j, i) != m.At(i, j) {
-				fmt.Println(n.At(j, i))
-				t.Errorf("transpose.At(%v, %v) is %v, but expecting %v", i, j, n.At(j, i), m.At(i, j))
+			if n[j][i] != m[i][j] {
+				t.Errorf("T[%v][%v] is %v, but expecting %v", i, j, n[j][i], m[i][j])
 			}
 		}
 	}
-	if !m.Equals(m.Transpose().Transpose()) {
+	if !Equal(m, T(T(m))) {
 		t.Errorf("mat.T.T != mat")
 	}
 }
@@ -108,78 +99,46 @@ func TestTimes(t *testing.T) {
 		col = 7
 	)
 	m := New(row, row)
-	q := Identity(row)
-	if !m.Times(q).Equals(m) {
+	q := I(row)
+	if !Equal(Times(m, q), m) {
 		t.Errorf("A Square matrix times the identity matrix should be equal to itself")
 	}
 	m = New(row, col)
 	n := New(row, col)
-	for i := 0; i < row*col; i++ {
-		m.Vals[i] = float64(i)
-	}
-	o := m.Times(n)
-	for i := 0; i < row*col; i++ {
-		if o.Vals[i] != 0.0 {
-			t.Errorf("Times product with 0 matrix, expect 0.0, got %v", o.Vals[i])
+	iter := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			m[i][j] = float64(iter)
+			iter++
 		}
 	}
-	o = m.Times(m)
-	p := m.Apply(func(i float64) float64 { return i * i })
-	if !o.Equals(p) {
+	o := Times(m, n)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if o[i][j] != 0.0 {
+				t.Errorf("o[%v][%v] == %v, expected 0.0", i, j, o[i][j])
+			}
+		}
+	}
+	o = Times(m, m)
+	m = Apply(func(i float64) float64 { return i * i }, m)
+	if !Equal(o, m) {
 		t.Errorf("m times m != m.Apply( i * i for each element i in m)")
 	}
 }
 
-func TestTimesInPlace(t *testing.T) {
-	var (
-		row = 5
-		col = 7
-	)
-	m := New(row, row)
-	q := Identity(row)
-	if !m.Times(q).Equals(m.TimesInPlace(q)) {
-		t.Errorf("A Square matrix times the identity matrix should be equal to itself")
-	}
-	m = New(row, col)
-	n := New(row, col)
-	for i := 0; i < row*col; i++ {
-		m.Vals[i] = float64(i)
-	}
-	m.TimesInPlace(n)
-	for i := 0; i < row*col; i++ {
-		if m.Vals[i] != 0.0 {
-			t.Errorf("TimesInPlace product with 0 matrix, expect 0.0, got %v", m.Vals[i])
-		}
-	}
-	for i := 0; i < row*col; i++ {
-		n.Vals[i] = float64(i)
-	}
-	p := n.Apply(func(i float64) float64 { return i * i })
-	n.TimesInPlace(n)
-	for i := 0; i < row*col; i++ {
-		if n.Vals[i] != p.Vals[i] {
-			t.Errorf("Times product matrix with itself, expect %v, got %v", p.Vals[i], n.Vals[i])
-		}
-	}
-
-}
-
 func TestApply(t *testing.T) {
-	m := New(4, 4)
-	n := m.Apply(func(i float64) float64 { return i + 1.0 })
-	for i := 0; i < 16; i++ {
-		if n.Vals[i] != 1.0 {
-			t.Errorf("expected 1.0, got %v", n.Vals[i])
-		}
-	}
-}
-
-func TestApplyInPlace(t *testing.T) {
-	m := New(4, 4)
-	m.ApplyInPlace(func(i float64) float64 { return i + 1.0 })
-	for i := 0; i < 16; i++ {
-		if m.Vals[i] != 1.0 {
-			t.Errorf("expected 1.0, got %v", m.Vals[i])
+	var (
+		row = 4
+		col = 4
+	)
+	m := New(row, col)
+	n := Apply(func(i float64) float64 { return i + 1.0 }, m)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if n[i][j] != 1.0 {
+				t.Errorf("expected 1.0, got %v", n[i][j])
+			}
 		}
 	}
 }
@@ -191,18 +150,23 @@ func TestDot(t *testing.T) {
 	)
 	m := New(row, col)
 	n := New(col, row)
-	o := m.Dot(n)
-	if o.NumRows != row {
-		t.Errorf("m.Dot(n).numRows expected %v, got %v", row, m.NumRows)
+	o := Dot(m, n)
+	if len(o) != row {
+		t.Errorf("Dot(m, n)'s numRows expected %v, got %v", row, len(o))
 	}
-	if o.NumCols != row {
-		t.Errorf("m.Dot(n).numCols expected %v, got %v", row, m.NumCols)
+	if len(o[0]) != row {
+		t.Errorf("Dot(m, n)'s numCols expected %v, got %v", row, len(o[0]))
 	}
-	for i := 0; i < row*row; i++ {
-		o.Vals[i] = float64(i)
+	iter := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < row; j++ {
+			o[i][j] = float64(iter)
+			iter++
+		}
 	}
-	if !o.Dot(Identity(row)).Equals(o) {
-		t.Errorf("mat64 x identity != mat64...")
+	p := Dot(I(row), o)
+	if !Equal(p, o) {
+		t.Errorf("o x I != o...")
 	}
 }
 
@@ -212,13 +176,19 @@ func TestReset(t *testing.T) {
 		col = 13
 	)
 	m := New(row, col)
-	for i := 0; i < row*col; i++ {
-		m.Vals[i] = float64(i)
+	iter := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			m[i][j] = float64(iter)
+			iter++
+		}
 	}
-	m.Reset()
-	for i := 0; i < row*col; i++ {
-		if m.Vals[i] != 0.0 {
-			t.Errorf("m.Reset at %v is equal %v", i, m.Vals[i])
+	m = Reset(m)
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if m[i][j] != 0.0 {
+				t.Errorf("Reset(m) at m[%v][%v] is equal %v", i, j, m[i][j])
+			}
 		}
 	}
 }
