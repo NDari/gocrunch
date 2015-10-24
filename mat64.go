efb5242c437af4e3af2d1a6fca69a846ec0d8fa1
Naseer Dari <naseerdari01@gmail.com>
2015-10-24 11:21:05 -0500

    Enhance package documentation
    Added a bit more meat to the package documentation, including how errors
    are handled, and what happening when things go wrong (we die).

--- mat64.go
+++ mat64.go
@@ -1,5 +1,15 @@
 // Package mat64 supplies functions that create or act
-// on 2D slices of float64s, for the Go language.
+// on 2D slices of float64s.
+//
+// The 2D slices acted on or created by the functions below are assumed
+// to be non-jagged. This means that for a given [][]float64, the inner
+// slices are assumed to be of the same length.
+//
+// All errors encountered in this package, such as shape mismatch between
+// two arrays to be multiplied together in an element-wise fashion, are
+// considered fatal. In other words, when encountering an error, the functions
+// in this package will die, instead of returning an error to be handled by
+// the caller.
 package mat64
 
 import (
