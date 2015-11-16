b5ec50d52a71dabef7112f1175c2da7cd3d5f504
Naseer Dari <naseerdari01@gmail.com>
2015-11-15 22:06:06 -0600

    Fixed mul. Also removed the need for double checking of vector bounds in mat.Mul since its done in vec.Mul

--- mat/mat.go
+++ mat/mat.go
@@ -205,22 +205,17 @@ of two 2D slices.
 */
 func Mul(m, n [][]float64) [][]float64 {
 	if len(m) != len(n) {
-		msg := "mat.%v Error: in %v [%v line %v].\n"
-		msg += "Number of rows of the first 2D slice is %v, while the number\n"
-		msg += "of rows of the second 2D slice is %v. They must match.\n"
-		p, f, l, _ := runtime.Caller(1)
-		log.Fatalf(msg, "Mul", f, runtime.FuncForPC(p).Name(), l, len(m), len(n))
+		fmt.Println("\nNumgo/mat error.")
+		s := "In mat.%s the number of rows of the first 2D slice is %d, while\n"
+		s += "the number of rows of the second 2D slice is %d. They must be equal\n"
+		s = fmt.Sprintf(s, "Mul", len(m), len(n))
+		fmt.Println(s)
+		fmt.Println("Stack trace for this error:\n")
+		debug.PrintStack()
+		os.Exit(1)
 	}
 	o := make([][]float64, len(m))
 	for i := 0; i < len(m); i++ {
-		if len(m[i]) != len(n[i]) {
-			msg := "mat.%v Error: in %v [%v line %v].\n"
-			msg += "In column %v, number of elements for the first 2D slice is %v,\n"
-			msg += "while the number of elements of the second 2D slice is %v.\n"
-			msg += "They must match.\n"
-			p, f, l, _ := runtime.Caller(1)
-			log.Fatalf(msg, "Mul", f, runtime.FuncForPC(p).Name(), l, i, len(m[i]), len(n[i]))
-		}
 		o[i] = vec.Mul(m[i], n[i])
 	}
 	return o
