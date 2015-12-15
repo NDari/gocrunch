5d94154b2bdf770685f470257ec3014f86cd083f
Naseer Dari <naseerdari01@gmail.com>
2015-12-15 07:05:38 -0600

    removed the test for Bare

--- mat/mat_test.go
+++ mat/mat_test.go
@@ -24,30 +24,6 @@ func TestNew(t *testing.T) {
 	}
 }
 
-// func TestBare(t *testing.T) {
-// 	rows := 13
-// 	cols := 7
-// 	m := Bare(rows, cols)
-// 	if m.r != rows {
-// 		t.Errorf("New mat.r is %d, expected %d", m.r, rows)
-// 	}
-// 	if m.c != cols {
-// 		t.Errorf("New mat.c is %d, expected %d", m.c, cols)
-// 	}
-// 	if m.vals == nil {
-// 		t.Errorf("Bare mat.vals not initilized")
-// 	}
-// 	if m.work != nil {
-// 		t.Errorf("Bare mat.work is initilized")
-// 	}
-// 	if len(m.vals) != rows*cols {
-// 		t.Errorf("len(mat.vals) is %d, expected %d", len(m.vals), rows*cols)
-// 	}
-// 	if len(m.work) != 0 {
-// 		t.Errorf("len(mat.work) is %d, expected 0", len(m.work))
-// 	}
-// }
-
 func TestIsJagged(t *testing.T) {
 	s := make([][]float64, 10)
 	for i := range s {
