9a90dc419e93c5f1efa63307bec27a56d5a9cbf5
Naseer Dari <naseerdari01@gmail.com>
2015-12-20 14:26:20 -0600

    Added Sum, without its test

--- mat/mat.go
+++ mat/mat.go
@@ -397,7 +397,7 @@ number of rows of the original mat, and the number of columns is equal to 1.
 func (m *mat) Col(x int) *mat {
 	if (x >= m.c) || (x < 0) {
 		fmt.Println("\nNumgo/mat error.")
-		s := "In mat.%s the requested column %d is outside of bounds [0, %d]\n"
+		s := "In mat.%s the requested column %d is outside of bounds [0, %d)\n"
 		s = fmt.Sprintf(s, "Col", x, m.c)
 		fmt.Println(s)
 		fmt.Println("Stack trace for this error:")
@@ -419,7 +419,7 @@ the number of columns is equal to the number of columns of the original mat.
 func (m *mat) Row(x int) *mat {
 	if (x >= m.r) || (x < 0) {
 		fmt.Println("\nNumgo/mat error.")
-		s := "In mat.%s the requested row %d is outside of the bounds [0, %d]\n"
+		s := "In mat.%s the requested row %d is outside of the bounds [0, %d)\n"
 		s = fmt.Sprintf(s, "Row", x, m.r)
 		fmt.Println(s)
 		fmt.Println("Stack trace for this error:")
@@ -579,8 +579,49 @@ func (m *mat) Scale(f float64) *mat {
 	return m
 }
 
-func (m *mat) Sum() {
-	return
+func (m *mat) Sum(axis, slice int) float64 {
+	if axis != 0 || axis != 1 {
+		fmt.Println("\nNumgo/mat error.")
+		s := "In mat.%v, the first argument must be 0 or 1, however %d "
+		s += "was recieved.\n"
+		s = fmt.Sprintf(s, "Sum", axis)
+		fmt.Println(s)
+		fmt.Println("Stack trace for this error:")
+		debug.PrintStack()
+		os.Exit(1)
+	}
+	if axis == 0 {
+		if (slice >= m.r) || (slice < 0) {
+			fmt.Println("\nNumgo/mat error.")
+			s := "In mat.%s the requested row %d is outside of bounds [0, %d)\n"
+			s = fmt.Sprintf(s, "Sum", slice, m.r)
+			fmt.Println(s)
+			fmt.Println("Stack trace for this error:")
+			debug.PrintStack()
+			os.Exit(1)
+		}
+	} else if axis == 1 {
+		if (slice >= m.c) || (slice < 0) {
+			fmt.Println("\nNumgo/mat error.")
+			s := "In mat.%s the requested row %d is outside of bounds [0, %d)\n"
+			s = fmt.Sprintf(s, "Sum", slice, m.c)
+			fmt.Println(s)
+			fmt.Println("Stack trace for this error:")
+			debug.PrintStack()
+			os.Exit(1)
+		}
+	}
+	x := 0.0
+	if axis == 0 {
+		for i := 0; i < m.c; i++ {
+			x += m.vals[slice*m.c+i]
+		}
+	} else if axis == 1 {
+		for i := 0; i < m.r; i++ {
+			x += m.vals[i*m.c+slice]
+		}
+	}
+	return x
 }
 
 func (m *mat) Average() {
