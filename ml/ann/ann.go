0a41beb44128fd06c8b864ee1b7497d5d2afd050
Naseer Dari <naseerdari01@gmail.com>
2016-01-17 13:42:56 -0600

    Fixed the New function

--- ml/ann/ann.go
+++ ml/ann/ann.go
@@ -17,19 +17,21 @@ Net is the main type of this package. It represents a fully connected artificial
 neural network.
 */
 type Net struct {
-	input  *mat.Mat
-	hidden []*mat.Mat
-	output *mat.Mat
-	bias   *mat.Mat
+	input     *mat.Mat
+	hidden    []*mat.Mat
+	output    *mat.Mat
+	bias      *mat.Mat
+	weights   []*mat.Mat
+	numLayers int
 }
 
 /*
 New is the main contructor of this package.
-TODO: weights are 2D. 3 input, 5 hidden => 15 weights.
 */
 func New(dims ...int) *Net {
 	net := &Net{}
-	switch len(dims) {
+	numLayers := len(dims)
+	switch numLayers {
 	case 0, 1, 2:
 		fmt.Println("\nNumgo/ann error.")
 		s := "In ann.%s, the number of inputs must be at least 3, but\n"
@@ -51,28 +53,28 @@ func New(dims ...int) *Net {
 			debug.PrintStack()
 			os.Exit(1)
 		}
-		if dims[len(dims)-1] < 1 {
+		if dims[numLayers-1] < 1 {
 			fmt.Println("\nNumgo/ann error.")
 			s := "In ann.%s, the number of nodes of the output layer must be\n"
 			s += "set to one or more. However, %d nodes were requested.\n"
-			s = fmt.Sprintf(s, "New", dims[len(dims)-1])
+			s = fmt.Sprintf(s, "New", dims[numLayers-1])
 			fmt.Println(s)
 			fmt.Println("Stack trace for this error:")
 			debug.PrintStack()
 			os.Exit(1)
 		}
 		inp := mat.New(1, dims[0])
-		out := mat.New(1, dims[len(dims)-1])
+		out := mat.New(1, dims[numLayers-1])
 		var hid []*mat.Mat
 		// exclude first and last int passed to this function, as they are the
 		// input and output layers.
-		for i := 1; i < len(dims)-1; i++ {
+		for i := 1; i < numLayers-1; i++ {
 			if dims[i] < 1 {
 				fmt.Println("\nNumgo/ann error.")
 				s := "In ann.%s, the number of nodes of hidden layer %d\n"
 				s += "layer must be set to one or more.\n"
 				s += "However, %d nodes were requested for this layer.\n"
-				s = fmt.Sprintf(s, "New", i, dims[len(dims)-1])
+				s = fmt.Sprintf(s, "New", i, dims[numLayers-1])
 				fmt.Println(s)
 				fmt.Println("Stack trace for this error:")
 				debug.PrintStack()
@@ -81,24 +83,23 @@ func New(dims ...int) *Net {
 			hid = append(hid, mat.New(1, dims[i]))
 		}
 		// one bias per hidden layer
-		bias := mat.New(1, len(dims)-2)
+		bias := mat.New(1, numLayers-2)
 
-		// Initialize the weights. We use http://arxiv.org/abs/1206.5533
+		// set and nitialize the weights. We use http://arxiv.org/abs/1206.5533
 		// for setting the random range.
-		for i := 0; i < len(dims)-2; i++ {
-			if i == 0 {
-				val := 4.0 * math.Sqrt(6.0/float64(dims[0]+dims[2]))
-				hid[0].Rand(-val, val)
-				continue
-			}
-			val := 4.0 * math.Sqrt(6.0/float64(dims[i-1]+dims[i+1]))
-			hid[i].Rand(-val, val)
+		var weights []*mat.Mat
+		for i := 1; i < numLayers; i++ {
+			val := 4.0 * math.Sqrt(6.0/float64(dims[i-1]+dims[i]))
+			w := mat.New(dims[i-1], dims[i]).Rand(-val, val)
+			weights = append(weights, w)
 		}
 		net = &Net{
 			inp,
 			hid,
 			out,
 			bias,
+			weights,
+			numLayers,
 		}
 	}
 	return net
@@ -109,10 +110,14 @@ Print writes the weights of the hidden layers to a file.
 TODO: Change this to actually print to a file, not stdout.
 */
 func (n *Net) Print() {
-	for i := 0; i < len(n.hidden); i++ {
-		fmt.Printf("Hidden layer %d weights:\n", i)
-		n.hidden[i].Print()
+	var str string
+	for i := 0; i < len(n.weights); i++ {
+		s := "Hidden layer %d weights:\n"
+		s = fmt.Sprintf(s, i)
+		str += s
+		str += n.weights[i].ToString()
 	}
-	fmt.Println("The bias weights:")
-	n.bias.Print()
+	str += "The bias weights:\n"
+	str += n.bias.ToString()
+	fmt.Println(str)
 }
