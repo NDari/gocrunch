287fa52d4316de3111c3139f40b75a7dbc41826c
Naseer Dari <naseerdari01@gmail.com>
2016-04-01 13:30:20 -0500

    Added a few test to vec"

--- vec/vec_test.go
+++ vec/vec_test.go
@@ -12,3 +12,41 @@ func TestPop(t *testing.T) {
 		t.Errorf("expected length of 1, got %d", len(v))
 	}
 }
+
+func TestPush(t *testing.T) {
+	val := 3.0
+	length := 3
+	v := make([]float64, length)
+	v = Push(v, val)
+	if len(v) != 4 {
+		t.Errorf("expected length of 4, got %d", len(v))
+	}
+	if v[length] != val {
+		t.Errorf("expected %f, got %f", val, v[length])
+	}
+}
+
+func TestShift(t *testing.T) {
+	v := []float64{1.0, 2.0, 3.0, 4.0}
+	x, v := Shift(v)
+	if x != 1.0 {
+		t.Errorf("expected 1.0, got %f", x)
+	}
+	if len(v) != 3 {
+		t.Errorf("expected length of 3, got %d", len(v))
+	}
+	if v[0] != 2.0 {
+		t.Errorf("expected first element to be 2.0, got %f", v[0])
+	}
+}
+
+func TestSUnshift(t *testing.T) {
+	v := []float64{1.0, 2.0, 3.0, 4.0}
+	v = Unshift(v, 0.0)
+	if len(v) != 5 {
+		t.Errorf("expected length of 5, got %d", len(v))
+	}
+	if v[0] != 0.0 {
+		t.Errorf("expected first element to be 0.0, got %f", v[0])
+	}
+}
